4a7aa6f14803662cbb4214acd9f56bfcdf3d9ac1
volodya0 <volodauzuk02@gmail.com>
2024-06-10 23:19:42 +0300

    formatting

--- autoclicker.py
+++ autoclicker.py
@@ -14,10 +14,11 @@ import random
 
 from mousemove import human_like_mouse_move
 
+
 def draw_lines_around_item(left, top, right, bottom):
     hwnd = win32gui.GetDesktopWindow()
     dc = win32gui.GetWindowDC(hwnd)
-    
+
     # Create a pen for drawing
     pen = win32gui.CreatePen(win32con.PS_SOLID, 3, win32api.RGB(0, 255, 0))
     old_pen = win32gui.SelectObject(dc, pen)
@@ -33,13 +34,15 @@ def draw_lines_around_item(left, top, right, bottom):
     win32gui.SelectObject(dc, old_pen)
     win32gui.DeleteObject(pen)
     win32gui.ReleaseDC(hwnd, dc)
-    
+
+
 def filter_unique_points(points, min_distance):
     unique_points = []
     for pt in points:
         if all(np.linalg.norm(np.array(pt) - np.array(unique_pt)) > min_distance for unique_pt in unique_points):
             unique_points.append(pt)
     return unique_points
+
 
 def match_template(gray_screenshot, template, threshold, min_distance):
     res = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
@@ -48,53 +51,57 @@ def match_template(gray_screenshot, template, threshold, min_distance):
     unique_points = filter_unique_points(match_points, min_distance)
     return unique_points
 
+
 def euclidean_distance(pt1, pt2):
     return ((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2) ** 0.5
 
 
 def nextStep():
-    if not ensureHead(): return
+    if not ensureHead():
+        return
     mouse_x, mouse_y = pyautogui.position()
     # regSize = 500
-    
+
     regX = headPt[0]
     regY = headPt[1]
-    
-    scr = pyautogui.screenshot(region=(int(regX), int(regY + headHeight),int( headWidth),int( 750)))
-    gray_scr = cv2.cvtColor(cv2.cvtColor(np.array(scr), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
-    
-    draw_lines_around_item(regX - 10, regY + headHeight,regX + headWidth + 20, regY +750)
-    
+
+    scr = pyautogui.screenshot(region=(int(regX), int(
+        regY + headHeight), int(headWidth), int(750)))
+    gray_scr = cv2.cvtColor(cv2.cvtColor(
+        np.array(scr), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
+
+    draw_lines_around_item(regX - 10, regY + headHeight,
+                           regX + headWidth + 20, regY + 750)
+
     unique_points1 = match_template(gray_scr, i1_t, threshold, min_distance)
     unique_points2 = match_template(gray_scr, i2_t, threshold, min_distance)
     unique_points3 = match_template(gray_scr, d1_t, threshold, min_distance)
 
     unique_points = unique_points1 + unique_points2 + unique_points3
-    
+
     if unique_points:
-        nearest = min(unique_points, key=lambda pt: euclidean_distance((pt[0] + w/2, pt[1]+ h/2), (mouse_x - regX, mouse_y - regY)))
+        nearest = min(unique_points, key=lambda pt: euclidean_distance(
+            (pt[0] + w/2, pt[1] + h/2), (mouse_x - regX, mouse_y - regY)))
         item_x, item_y = nearest
         item_x += regX
         item_y += regY
-        
+
         start_time = time.time()
-        
+
         if drawing:
-            
+
             draw_lines_around_item(item_x, item_y, item_x + w, item_y + h)
-            
+
         print(f"drawing time: {( time.time() - start_time) * 1000} ms")
-        
-        
+
         start_time = time.time()
-        
+
         if moving:
-            pyautogui.click(item_x + random.uniform(0, w), item_y + random.uniform(0, h)) 
+            pyautogui.click(item_x + random.uniform(0, w),
+                            item_y + random.uniform(0, h))
             # human_like_mouse_move((mouse_x, mouse_y), (item_x + w / 2, item_y + h / 2))
-            
+
         print(f"moving time: {( time.time() - start_time) * 1000} ms")
-        
-            
 
 
 # Function to perform the clicking and draw lines around detected items
@@ -102,12 +109,12 @@ def run():
     while True:
         if running:
             print('running...')
-            nextStep()     
+            nextStep()
         else:
             print('waiting...')
 
-        
         time.sleep(0.01)
+
 
 def toggle_run():
     global running, click_thread
@@ -116,25 +123,29 @@ def toggle_run():
         click_thread = threading.Thread(target=run)
         click_thread.start()
 
+
 def toggle_moving():
     global moving
     moving = not moving
 
+
 def toggle_drawing():
     global drawing
     drawing = not drawing
-    
+
+
 def ensureHead():
     # if not headPt:
-        global headPt
-        gray_scr = cv2.cvtColor(cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
-    
-        matches = match_template(gray_scr, head_t, threshold, min_distance)
-        if matches:
-            headPt = (matches[0][0], matches[0][1])
-            return True
+    global headPt
+    gray_scr = cv2.cvtColor(cv2.cvtColor(
+        np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2GRAY)
+
+    matches = match_template(gray_scr, head_t, threshold, min_distance)
+    if matches:
+        headPt = (matches[0][0], matches[0][1])
+        return True
     # return False
-    
+
 
 head_t = cv2.imread('templates_png/head.png', 0)
 headWidth, headHeight = head_t.shape[::-1]
@@ -163,7 +174,7 @@ if __name__ == "__main__":
     keyboard.add_hotkey('s', toggle_run)
     keyboard.add_hotkey('m', toggle_moving)
     keyboard.add_hotkey('d', toggle_drawing)
-    
+
     toggle_run()
 
     # Keep the program running
--- mousemove.py
+++ mousemove.py
@@ -3,22 +3,26 @@ import pyautogui
 import numpy as np
 import random
 
+
 def bezier_curve(start, control, end, num_points=3):
     t_values = [i / (num_points - 1) for i in range(num_points)]
     curve_points = [
         (
-            (1 - t) ** 2 * start[0] + 2 * (1 - t) * t * control[0] + t ** 2 * end[0],
-            (1 - t) ** 2 * start[1] + 2 * (1 - t) * t * control[1] + t ** 2 * end[1]
+            (1 - t) ** 2 * start[0] + 2 * (1 - t) *
+            t * control[0] + t ** 2 * end[0],
+            (1 - t) ** 2 * start[1] + 2 * (1 - t) *
+            t * control[1] + t ** 2 * end[1]
         )
         for t in t_values
     ]
     return curve_points
 
+
 def human_like_mouse_move(start, end):
     control1 = (start[0] + (end[0] - start[0]) * random.uniform(0.2, 0.8) + random.uniform(-20, 20),
                 start[1] + (end[1] - start[1]) * random.uniform(0.2, 0.8) + random.uniform(-20, 20))
-    
+
     points = bezier_curve(start, control1, end, num_points=3)
-    
+
     for point in points:
         pyautogui.moveTo(point[0], point[1])  #
